e4fa185e73bb4ced80d0379682dad805eef121a0
Wendy <wendygoodwin@mac.com>
2016-05-06 23:42:39 -0700

    responsive images and layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,26 +5,33 @@ console.log('\'Allo \'Allo!');
 // http://kenwheeler.github.io/slick/
 $(document).ready(function(){
   $('.old-piano-slider').slick({
-
     slidesToShow: 3,
     slidesToScroll: 1,
     variableWidth: true
   });
 
+  $('.typography-items').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
+
+  $('.studioPics').slick({
+    })
+
   $('.bigMap-slideshow').slick({
-    autoplay: true
+      autoplay: true
+    });
+
+  $('.sprayFade').slick({
+    // dots: false,
+    // infinite: true,
+    // speed: 200,
+    // fade: true,
+    // cssEase: 'linear',
+    // autoplay: true,
+    // pauseOnHover: true
   });
 
 
-
-
-$('.sprayFade').slick({
-  dots: true,
-  infinite: true,
-  speed: 200,
-  fade: true,
-  cssEase: 'linear',
-  autoplay: true,
-  pauseOnHover: true
-});
 });
